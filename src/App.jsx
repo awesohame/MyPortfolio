9c131ed11024769cf6ee5f7cf292a0176389ef33
Awesohame <82087247+awesohame@users.noreply.github.com>
2023-12-09 19:36:23 +0530

    Replaced SVG with FA React component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Skills from './pages/Skills';
+import Contact from './pages/Contact';
 
 function App() {
 
@@ -17,7 +18,7 @@ function App() {
         <Route exact path='/about' Component={About} />
         <Route exact path='/projects' Component={Projects} />
         <Route exact path='/skills' Component={Skills} />
-        <Route exact path='/contact' element='Contact' />
+        <Route exact path='/contact' Component={Contact} />
       </Routes>
     </>
   )
