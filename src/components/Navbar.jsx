e9b5930a7eda32b508bea1f68db98e99801204d2
Awesohame <82087247+awesohame@users.noreply.github.com>
2023-12-10 15:51:46 +0530

    made Navbar collapsible

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import './Navbar.css';
 import navimg from '../assets/SA.png';
 import { NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faCircleUser, faDiagramProject, faBrain, faComment } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faCircleUser, faDiagramProject, faBrain, faComment, faCircleChevronLeft, faCircleChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 let navdata = [
     {
@@ -33,12 +34,24 @@ let navdata = [
 ]
 
 export default function Navbar() {
+    const [expanded, setExpanded] = useState(true);
+    //h-screen shrink-0 bg-[#24282d] basis-[5%]
     return (
-        <div className="h-screen basis-[15%] shrink-0 bg-[#24282d]">
-            <div className="flex flex-col items-center w-full p-1 h-1/5">
-                <img src={navimg} alt="img" className="h-20 w-20 m-6" />
-            </div>
+        <div className={`h-screen shrink-0 bg-[#24282d] ${expanded ? "basis-[15%]" : "basis-[5%]"}`}>
+            <div className="flex justify-end items-center w-full h-1/5">
+                {expanded && (<div className='flex justify-center items-center w-full'>
+                    <img src={navimg} alt="img" className="h-20 w-20" />
+                </div>)}
+                <div className='p-4'>
+                    <button onClick={() => {
+                        setExpanded(!expanded);
+                    }} className='h-10 w-10 text-[#eeeeee] cursor-pointer'>
+                        {expanded ? <FontAwesomeIcon icon={faCircleChevronLeft} className='h-full w-full' /> : <FontAwesomeIcon icon={faCircleChevronRight} className='h-full w-full' />}
+                    </button>
 
+                </div>
+
+            </div>
             <div className="h-3/5 flex flex-col justify-center">
                 <ul className="flex flex-col items-center">
                     {navdata.map((item, index) => (
@@ -46,13 +59,12 @@ export default function Navbar() {
                             <NavLink to={item.link}
                                 // className='rounded-lg text-2xl/10 p-3 m-2 w-3/4 flex items-center hover:bg-[#1a1b1b]'
                                 className={({ isActive }) => {
-                                    return ('rounded-lg text-2xl/10 p-3 m-2 w-3/4 flex items-center hover:bg-[#1a1b1b] '
-                                        +
-                                        ((isActive) ? ' bg-[#1a1b1b]' : ' bg-[#24282d]')
+                                    return ('rounded-lg text-2xl/8 p-4 m-1 w-[80%] flex items-center hover:bg-[#1a1b1b] '
+                                        + ((isActive) ? ' bg-[#1a1b1b]' : ' bg-[#24282d]')
                                     )
                                 }}>
                                 <FontAwesomeIcon icon={item.icon} className='h-8 w-8 text-[#eeeeee]' />
-                                <p className="pl-3 text-[#eeeeee]">{item.name}</p>
+                                {expanded && (<p className="pl-3 text-[#eeeeee]">{item.name}</p>)}
                             </NavLink>
                         </li>
                     ))}
@@ -68,7 +80,7 @@ export default function Navbar() {
                             ((isActive) ? ' bg-[#ffc54d] text-[#1a1b1b]' : ' bg-[#1a1b1b] text-[#eeeeee]')
                         )
                     }}>
-                    Resume
+                    {expanded ? ('Resume') : ('R')}
                 </NavLink>
             </div>
         </div >
